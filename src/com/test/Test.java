fa954f4906769ec87c629df1f902951725652801
vijji999 <vijaya.saragadam@gmail.com>
2018-07-07 12:47:16 +0530

    added new code

---
+++ src/com/test/Test.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Test {
+	public static void main(String args[]) {
+	 System.setProperty("webdriver.chrome.driver","C:/Users/ss/Downloads/chromedriver.exe");
+	 WebDriver driver = new ChromeDriver();
+	 WebDriverWait wait = new WebDriverWait(driver, 15);
+	 driver.get("https://www.google.co.in/");
+	 driver.manage().window().maximize();
+	 driver.findElement(By.id("lst-ib")).sendKeys("flipkart");
+	 driver.quit();
+	 
+			 
+
+}}
