e40409723a586734e6acf8f1d6f51fbbdb0b2231
vijji999 <vijaya.saragadam@gmail.com>
2018-07-07 12:54:18 +0530

    comment a line

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -1,7 +1,5 @@
 package com.test;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,7 +9,7 @@ public class Test {
 	public static void main(String args[]) {
 	 System.setProperty("webdriver.chrome.driver","C:/Users/ss/Downloads/chromedriver.exe");
 	 WebDriver driver = new ChromeDriver();
-	 WebDriverWait wait = new WebDriverWait(driver, 15);
+//	 WebDriverWait wait = new WebDriverWait(driver, 15);
 	 driver.get("https://www.google.co.in/");
 	 driver.manage().window().maximize();
 	 driver.findElement(By.id("lst-ib")).sendKeys("flipkart");
